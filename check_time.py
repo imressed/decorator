d7dd3dd79d81528378356b73c79da55f490165ab
imressed <imressed@gmail.com>
2015-04-17 12:32:36 +0300

    add decorator func for calculating func working time

--- check_time.py
+++ check_time.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 __author__ = 'imressed'
+
+import time
+
+
+def timer(f):
+    def tmp(*args, **kwargs):
+        t = time.time()
+        res = f(*args, **kwargs)
+        print ("Func working time: %f" % (time.time() - t))
+        return res
+    return tmp
+
+@timer
+def func(x, y):
+    return x + y
+
+print(func(2,5))
