3a35f2979bc41ad601d07248b6ecbeba64141774
imressed <imressed@gmail.com>
2015-04-17 12:43:58 +0300

    add pause decorator

--- check_time.py
+++ check_time.py
@@ -4,15 +4,25 @@ __author__ = 'imressed'
 import time
 
 
+def pause(t = 1):
+    def wrapper(f):
+        def tmp(*args, **kwargs):
+            time.sleep(t)
+            return f(*args, **kwargs)
+        return tmp
+    return wrapper
+
+
 def timer(f):
-    def tmp(*args, **kwargs):
+    def wrap(*args, **kwargs):
         t = time.time()
         res = f(*args, **kwargs)
         print ("Func working time: %f" % (time.time() - t))
         return res
-    return tmp
+    return wrap
 
 @timer
+@pause(2)
 def func(x, y):
     return x + y
 
